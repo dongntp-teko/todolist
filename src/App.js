7f1ed5be6eab58cec552e43d3f347f9a698ef35a
dongntp-teko <dong.ntp@teko.vn>
2019-07-02 14:13:28 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -16,22 +16,28 @@ class TodoItem extends React.Component{
   constructor(props){
     super(props);
     this.handleRemove=this.handleRemove.bind(this);
+    this.handleChange=this.handleChange.bind(this);
   }
 
   handleRemove(){
-    //this.props.removeItem(parseInt(this.props.key));
-    alert(this.props.key);
+    this.props.removeItem(parseInt(this.props.key1));
+    //alert(parseInt(this.props.key1));
+  }
+
+  handleChange(){
+    this.props.markTodo(this.props.key1);
+    //alert(parseInt(this.props.key1));
   }
 
   render(){
     const item = (this.props.item.done) ? (
       <li class="list-group-item list-group-item-action text-decor">
-        <i class="fas fa-check"></i> {this.props.item.value}
+        <span onClick={this.handleChange}><i class="fas fa-check"></i></span> {this.props.item.value}
         <button type="button" onClick={this.handleRemove} class='remove btn btn-default'><i class="fas fa-times"></i></button>         
       </li>
     ): (
       <li class="list-group-item list-group-item-action  text-green">
-        <i class="fas fa-check"></i> {this.props.item.value}
+        <span onClick={this.handleChange}><i class="fas fa-check"></i></span> {this.props.item.value}
         <button type="button" onClick={this.handleRemove} class='remove btn btn-default'><i class="fas fa-times"></i></button>         
       </li>
     )
@@ -42,7 +48,7 @@ class TodoItem extends React.Component{
 class TodoList extends React.Component{
   render(){  
     const listItems=this.props.listItem.map((items, index)=>
-    <TodoItem item={items} key={index} removeItem={this.props.removeItem} />
+    <TodoItem item={items} key1={index} markTodo={this.props.markTodoDone} removeItem={this.props.removeItem} />
   );
     return(
       <ul className='list-group'>
@@ -84,6 +90,8 @@ class ShowContent extends React.Component{
   constructor(props){
     super(props);
     this.addItem=this.addItem.bind(this);
+    this.markTodoDone=this.markTodoDone.bind(this);
+    this.removeItem=this.removeItem.bind(this);
     this.state={todoItems: todoItems};
   }
 
@@ -93,14 +101,24 @@ class ShowContent extends React.Component{
   }
 
   removeItem(index){
-    todoItems.slice(index);
+    todoItems.splice(index,1);
     this.setState({todoItems: todoItems});
+  
+  }
+
+  markTodoDone(index){
+    var don=todoItems[index].done;
+    var val=todoItems[index].value;
+    todoItems.splice(index,1);
+    don ? todoItems.unshift({value: val, done: false}) : todoItems.push({value: val, done: true});
+    this.setState({todoItems: todoItems});
+   // alert(index);
   }
 
   render(){
     return (
       <div>
-        <TodoList listItem={this.props.todoItems} removeItem={this.removeItem} />
+        <TodoList listItem={this.props.todoItems} removeItem={this.removeItem} markTodoDone={this.markTodoDone} />
         <AddTodo addItem={this.addItem}/>
       </div>
     )
